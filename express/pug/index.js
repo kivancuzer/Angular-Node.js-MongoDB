4667a19305509b441b17bb11b0d9d08c97a05f59
kivancuzer <kivancuzer@gmail.com>
2020-08-04 13:10:30 +0300

    kivanc
    kivanc

--- express/pug/index.js
+++ express/pug/index.js
@@ -6,6 +6,12 @@ app.set("view engine", "pug");
 app.get("/", (req,res)=>{
     res.render("index", {name:"kivanc",surname:"uzer"});
 });
+app.get("/home", (req,res)=>{
+    res.render("home");
+});
+app.get("/contact", (req,res)=>{
+    res.render("contact");
+});
 
 app.listen(3000, ()=>{
     console.log("express server çalıştı");
