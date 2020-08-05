82dd00cb12eca3a8c3036dbef9f3fe8da0c09f44
kivancuzer <kivancuzer@gmail.com>
2020-08-05 12:26:11 +0300

    kivanc
    kivanc

--- mongodb/app.js
+++ mongodb/app.js
@@ -3,11 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoose.connect("mongodb://localhost/udemy",{useNewUrlParser: true,useUnifiedTopology: true})
+.then(()=>{
+  console.log("MongoDB bağlantısı sağlandı.");
+}).catch((err)=>{
+  console.log("MongoDB bağlantı hatası");
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
